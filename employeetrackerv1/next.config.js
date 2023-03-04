c693b7f20881e86c76d511ed7ebd3d8f20f22061
Andrico Sean <andrico@ark.ph>
2023-03-04 12:39:50 +0800

    register user fixes

--- employeetrackerv1/next.config.js
+++ employeetrackerv1/next.config.js
@@ -4,9 +4,10 @@ const nextConfig = {
 };
 module.exports = {
   env: {
-    mongodb_url: "asfasdf",
-    access_token_secret: `7978e18c7f6fe7c010437ed7675311d1b96a0675ae840156c71c01bd8be3ba9b`,
-    refresh_token_secret: `5615fdcc8984ebae3f7cd542f5da56338dc59452d7d9669575c17b231ff1b538`,
+    NEXT_PUBLIC_MONGODB_URL:
+      "mongodb+srv://admin:test1234@emplyeetracker.p9x6nfv.mongodb.net/test",
+    NEXT_PUBLIC_ACCESS_TOKEN_SECRET: `7978e18c7f6fe7c010437ed7675311d1b96a0675ae840156c71c01bd8be3ba9b`,
+    NEXT_PUBLIC_REFRESH_TOKEN_SECRET: `5615fdcc8984ebae3f7cd542f5da56338dc59452d7d9669575c17b231ff1b538`,
   },
 };
 
