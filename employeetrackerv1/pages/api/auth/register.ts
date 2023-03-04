37c77facf7ea018fab0289b7cfdd54cba38f0cb7
Andrico Sean <andrico@ark.ph>
2023-03-04 11:41:11 +0800

    added middleware and config

--- employeetrackerv1/pages/api/auth/register.ts
+++ employeetrackerv1/pages/api/auth/register.ts
@@ -1,8 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import * as _ from "lodash";
+const User = require("../models/User");
+const { signAccessToken } = require("../helpers/jwtHelper");
+import connectDB from "../middleware/mongodb";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     try {
       const email = req.body.email;
@@ -31,17 +34,33 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
         });
       }
 
-      //check db if email exist
-      //then save user to db
-      //TODO: setup db
+      const existingUser = await User.findOne({ email: email });
+
+      if (existingUser) {
+        return res.status(400).send({
+          success: false,
+          message: "User already exist",
+        });
+      }
+
+      const creationDate = new Date().toISOString();
+
+      const user = new User({
+        email,
+        password,
+        creationDate,
+      });
+
+      const savedUser = await user.save();
+      const accessToken = await signAccessToken(savedUser.id);
 
       return res.status(200).send({
         succcess: true,
         message: "Sucessfully Registered",
         data: {
           email,
-          creationDate: new Date().toISOString(),
-          //accessToken should be here
+          creationDate,
+          accessToken,
         },
       });
     } catch (error) {
@@ -60,3 +79,5 @@ function isEmail(email: string) {
 
   return false;
 }
+
+export default connectDB(handler);
